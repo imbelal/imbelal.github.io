224e80aaa9abfc5459bf0075ab0afa01c73ebada
imbelal <imbelalhosen@gmail.com>
2022-02-03 00:28:38 +0100

    Update - move right panel to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ParticlesConfig } from 'src/app/particles-config';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faXingSquare } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
+import { environment } from 'src/environments/environment';
+
 
 declare var particlesJS: any;
 
@@ -12,13 +21,44 @@ declare var particlesJS: any;
 export class AppComponent implements OnInit{
   
   title = 'imbelal_portfulio';
+  faMoon = faMoon;
+  faSun = faSun;
+  faLinkedin = faLinkedin;
+  faXing = faXingSquare;
+  faGithub = faGithub;
+  faMail = faEnvelope;
+  faMobile = faPhoneSquare;
+  email = environment.email;
+  mobile = environment.mobile;
+  mobileNo = `tel:${environment.mobile}`;
+
   
   public ngOnInit(): void {
     this.invokeParticles();
+    this.changeThemeColor();
   }
 
   public invokeParticles(): void {
     particlesJS('particles-js', ParticlesConfig, function() {});
+  }
+
+  toggleTheme(){
+    // Whenever the user explicitly chooses theme color
+    if(localStorage.getItem('theme') === 'dark')
+      localStorage.setItem('theme',''); // replacing dark string with empty string
+    else
+      localStorage.setItem('theme','dark');
+
+    this.changeThemeColor();
+  }
+
+  changeThemeColor(){
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
   
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faPalette } from '@fortawesome/free-solid-svg-icons';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import { faSun } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faXingSquare } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons';
-
 import Typed from 'typed.js';
-import { environment } from 'src/environments/environment';
-import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -18,22 +8,11 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  faPalette = faPalette;
-  faMoon = faMoon;
-  faSun = faSun;
-  faLinkedin = faLinkedin;
-  faXing = faXingSquare;
-  faGithub = faGithub;
-  faMail = faEnvelope;
-  faMobile = faPhoneSquare;
-  email = environment.email;
-  mobile = environment.mobile;
-  mobileNo = `tel:${environment.mobile}`;
+
 
   constructor() { }
 
   ngOnInit(): void {
-    this.changeThemeColor();
     const options_for_second_paragraph = {
       strings: [
         'A passionate full-stack software developer who focuses on writing clean, elegant and efficient code.',
@@ -46,25 +25,6 @@ export class HomeComponent implements OnInit {
       loop: true
     };
     const typed = new Typed('.second_typed-element', options_for_second_paragraph);
-  }
-
-  toggleTheme(){
-    // Whenever the user explicitly chooses theme color
-    if(localStorage.getItem('theme') === 'dark')
-      localStorage.setItem('theme',''); // replacing dark string with empty string
-    else
-      localStorage.setItem('theme','dark');
-
-    this.changeThemeColor();
-  }
-
-  changeThemeColor(){
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
   }
 
 }
